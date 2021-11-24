aaf18edb225135b1f8c9bd2f74cd826d0cf28347
ErriBrr <rer-b@laposte.net>
2021-11-24 14:46:09 +0100

    Il semble impossible de pouvoir utiliser ces string pour les import

---
+++ src/app/app-tree.ts
@@ -0,0 +1,20 @@
+export const APP_TREE = {
+    components: {
+        buttons: './buttons/buttons.component',
+        map: './map/map.component'
+    }, constants: {
+        continents: "./continents"
+    }, directives: { 
+
+    }, interfaces: {
+        feature: "./feature",
+        map_layer: "./map-layer"
+    }, services: {
+        features_data: "./features-data.service",
+        init_map: "./init-map.service",
+        map_controller: "./map-controller.service",
+        marker: "./marker.service",
+        popup: "./popup.service",
+        shape: "./shape.service"
+    }
+}
